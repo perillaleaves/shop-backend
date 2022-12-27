bb14ae2405ce47a0568bb49b0d1db1956827f781
gyunny <needle12s@gmail.com>
2022-12-27 17:02:06 +0900

    wip

--- src/main/java/perillaleaves/shop/repository/itemColor/WhiteRepository.java
+++
@@ -1,9 +0,0 @@
-package perillaleaves.shop.repository.itemColor;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import perillaleaves.shop.domain.itemColor.White;
-
-@Repository
-public interface WhiteRepository extends JpaRepository<White, Long> {
-}
