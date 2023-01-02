e01105ff3a6415d20313e2ab35b5ae8b34259e2f
gyunny <needle12s@gmail.com>
2023-01-02 13:12:52 +0900

    wip

--- src/main/java/perillaleaves/shop/controller/ItemController.java
+++ src/main/java/perillaleaves/shop/controller/ItemController.java
@@ -71,16 +71,11 @@ public class ItemController {
             List<ItemColorListResponse> itemColorResponse = new ArrayList<>();
 
             for (ItemColor itemColor : itemColors) {
-                ItemColorListResponse itemColorListResponse = new ItemColorListResponse(
-                        itemColor.getId(),
-                        itemColor.getColor());
+                ItemColorListResponse itemColorListResponse = new ItemColorListResponse(itemColor.getId(), itemColor.getColor());
                 itemColorResponse.add(itemColorListResponse);
             }
 
-            ItemListResponse itemListResponse = new ItemListResponse(item.getId(),
-                    item.getName(),
-                    item.getPrice(),
-                    itemColorResponse);
+            ItemListResponse itemListResponse = new ItemListResponse(item.getId(), item.getName(), item.getPrice(), itemColorResponse);
             itemResponse.add(itemListResponse);
 
         }
